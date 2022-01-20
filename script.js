f5ebdc4cb922e06d52a22404777955cc1c5ac462
Discusser <47938380+Discusser@users.noreply.github.com>
2022-01-20 22:53:46 +0100

    Added button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let apiKey = '1be9a6884abd4c3ea143b59ca317c6b2';
-console.log("hello")
-$.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
-    console.log(JSON.stringify(data, null, 2));
-});
+function buttonClick() {
+    alert("you clicked the button.")
+}
