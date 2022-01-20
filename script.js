225a3a9ea8e9de7af9e21736598ec6a7ae2b3fd8
Discusser <47938380+Discusser@users.noreply.github.com>
2022-01-20 23:24:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("HEllo")
 function displayCPP() {
     document.getElementById("home").style.visibility = "hidden";
     document.getElementById("projectNameCPP").style.visibility = "visible";
