73bd908e0ab0cd1dca57e29cfe6a54c332da83e5
Discusser <47938380+Discusser@users.noreply.github.com>
2022-01-21 20:00:27 +0100

    added dark mode

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function display(project) {
     document.getElementById(project).style.visibility = "visible";
     lastOpenedProject = project;
 }
+
+function toggleDarkMode() {
+    if (getComputedStyle(document.body).backgroundColor === "rgb(19, 20, 23)") {
+        document.body.style.backgroundColor = "#FFFFFF";
+    } else {
+        document.body.style.backgroundColor = "#131417";
+    }
+    document.querySelectorAll("h1, h2, h3, h4, h5, h6, p").forEach(value => {
+        console.log(value.style.color);
+       if (value.style.color === "rgb(19, 20, 23)") {
+           value.style.color = "#FFFFFF";
+       } else {
+           value.style.color = "#131417";
+       }
+    })
+}
