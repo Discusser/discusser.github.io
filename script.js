5a19f15b3e18e5ba08175f772c2c623cb00ae45a
Discusser <47938380+Discusser@users.noreply.github.com>
2022-01-22 11:39:16 +0100

    Fixed scroll

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let lastOpenedProject = null;
 
-function displayHome(currentPage) {
-    document.getElementById(currentPage).style.visibility = "hidden";
-    document.getElementById("home").style.visibility = "visible";
-}
-
 function display(project) {
     try {
-        document.getElementById(lastOpenedProject).style.visibility = "hidden";
+        document.getElementById(lastOpenedProject).style.display = "none";
     } catch (e) {}
-    document.getElementById(project).style.visibility = "visible";
+    document.getElementById(project).style.display = "initial";
     lastOpenedProject = project;
 }
 
