a143206d108232f2ed1dc968e2c2a094d92ef0bb
Discusser <47938380+Discusser@users.noreply.github.com>
2022-08-07 21:28:25 +0200

    fix tesseract

--- tesseract/script.js
+++ tesseract/script.js
@@ -1,10 +1,11 @@
 window.onload = () => {
     window.setInterval(function() {
-        let img = document.createElement("img")
-        img.src = "https://github.com/GTNewHorizons/GT5-Unofficial/blob/b3c3b9c5d7d0f40404f702b849d46f348cd78a9c/src/main/resources/assets/gregtech/textures/items/gt.metaitem.01/415.png?raw=true"
-        document.body.appendChild(img)
-        document.body.appendChild(document.createElement("br"))
-        console.log("hi")
-    }, 10)
+        for (i = 0; i < 188; i++) {
+            let img = document.createElement("img")
+            img.src = "../images/tesseract/frame_" + ("000" + i).slice(-3) + "_delay-0.03s.png"
+            document.body.appendChild(img)
+            document.body.appendChild(document.createElement("br"))
+        }
+    }, 100)
 
 }
