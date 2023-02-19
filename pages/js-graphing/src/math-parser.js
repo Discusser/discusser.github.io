adbbd67f83f03e4ca2e24aa22078ca02958f3c46
discusser <noobly321growing@gmail.com>
2023-02-19 21:00:01 +0100

    math parser progress

--- pages/js-graphing/src/math-parser.js
+++ pages/js-graphing/src/math-parser.js
@@ -6,14 +6,30 @@ var Associativity;
     Associativity[Associativity["LEFT"] = 0] = "LEFT";
     Associativity[Associativity["RIGHT"] = 1] = "RIGHT";
 })(Associativity || (Associativity = {}));
+class TFunction {
+    value;
+    onApply;
+    constructor(value, onApply) {
+        this.value = value;
+        this.onApply = onApply;
+    }
+    apply(args) {
+        return this.onApply.apply(null, args);
+    }
+}
 class Operator {
     value;
     precedence;
     associativity;
-    constructor(value, precedence, associativity) {
+    onApply;
+    constructor(value, precedence, associativity, onApply) {
         this.value = value;
         this.precedence = precedence;
         this.associativity = associativity;
+        this.onApply = onApply;
+    }
+    apply(a, b) {
+        return this.onApply(a, b);
     }
 }
 class Parenthesis {
@@ -34,23 +50,39 @@ const parentheses = new Map([
 ]);
 const operators = new Map([
     // Exponentiation
-    ["^", new Operator("^", 4, Associativity.RIGHT)],
+    ["^", new Operator("^", 4, Associativity.RIGHT, (a, b) => Math.pow(a, b))],
     // ["**", new Operator("**", 4, Associativity.RIGHT)],
     // Multiplication
-    ["*", new Operator("*", 3, Associativity.LEFT)],
+    ["*", new Operator("*", 3, Associativity.LEFT, (a, b) => a * b)],
     // ["×", new Operator("×", 3, Associativity.LEFT)],
     // Division
-    ["/", new Operator("/", 3, Associativity.LEFT)],
+    ["/", new Operator("/", 3, Associativity.LEFT, (a, b) => a / b)],
     // ["÷", new Operator("÷", 3, Associativity.LEFT)],
     // [":", new Operator(":", 3, Associativity.LEFT)],
     // Addition
-    ["+", new Operator("+", 2, Associativity.LEFT)],
+    ["+", new Operator("+", 2, Associativity.LEFT, (a, b) => a + b)],
     // Subtraction
-    ["-", new Operator("-", 2, Associativity.LEFT)],
+    ["-", new Operator("-", 2, Associativity.LEFT, (a, b) => a - b)],
 ]);
+const functions = new Map([]);
+const descriptors = Object.getOwnPropertyDescriptors(Math);
+const keys = Object.keys(descriptors);
+for (let i = 0; i < keys.length; i++) {
+    let value = descriptors[keys[i]].value;
+    if (typeof (value) === "function")
+        functions.set(keys[i], new TFunction(keys[i], value));
+}
 function isNumber(str) {
     try {
         return str.replace(str.match(/\d+\.*\d*/)[0], "").length === 0;
+    }
+    catch (e) {
+        return false;
+    }
+}
+function isVariable(str) {
+    try {
+        return str.replace(str.match(/[a-zA-Z]+'*/)[0], "").length === 0;
     }
     catch (e) {
         return false;
@@ -64,25 +96,57 @@ function isOperator(str) {
     return str.length === 1 && operators.has(str);
 }
 const mathParser = {
-    // 15 - 8 * 14.6
-    // 3+4*2/(1-5)^2^3
     // todo: support numbers written like -5 or +10
-    toTokenArray(expression) {
-        let expressionCopy = expression.replace(/\s/g, "") + " ";
+    toTokenArray(expression, constants = new Map(), userFunctions = new Map()) {
+        let expressionCopy = expression
+            .replace(/\s/g, "")
+            .replace(/(?<=\d)([a-zA-Z]'*)/g, "*$1")
+            + " ";
         let output = [];
         while (expressionCopy.length > 0) {
             let previous = "";
             for (let i = 1; i <= expressionCopy.length; i++) {
                 const slice = expressionCopy.slice(0, i);
-                if (isNumber(previous) && !isNumber(slice)) {
+                if ((isNumber(previous) && !isNumber(slice))) {
                     output.push(new Operand(previous));
                     break;
                 }
-                else if (isOperator(slice)) {
-                    const operator = operators.get(slice);
+                else if (isVariable(previous) && !isVariable(slice)) {
+                    if (slice.endsWith("(")) { // check if it's a function
+                        if (functions.has(previous)) { // @ts-ignore
+                            output.push(functions.get(previous));
+                            break;
+                        }
+                        else if (userFunctions.has(previous)) {
+                            // @ts-ignore
+                            let func = userFunctions.get(previous);
+                            if (typeof (func) === "string") {
+                                // todo : impl
+                                func = this.toFunction(previous, func, constants, userFunctions);
+                            }
+                            output.push(func);
+                            break;
+                        }
+                        else {
+                            throw new Error("Found invalid function " + previous);
+                        }
+                    }
+                    else if (constants.has(previous)) {
+                        // @ts-ignore
+                        let constant = constants.get(previous);
+                        const constantsCopy = constants;
+                        constantsCopy.delete(constant);
+                        output.push(...this.toTokenArray("(" + constant + ")", constantsCopy));
+                        break;
+                    }
+                    else {
+                        throw new Error("Could not find variable \"" + previous + "\"!");
+                    }
+                }
+                else if (isOperator(previous) && !isOperator(slice)) {
+                    const operator = operators.get(previous);
                     if (operator != undefined)
                         output.push(operator);
-                    previous = slice;
                     break;
                 }
                 else if (isParentheses(slice)) {
@@ -92,25 +156,33 @@ const mathParser = {
                     previous = slice;
                     break;
                 }
-                previous = slice;
+                else if (slice.length === expressionCopy.length) { // If nothing was found in the entire string
+                    expressionCopy = expressionCopy.slice(1);
+                }
+                else { // If nothing was found for the current slice
+                    previous = slice;
+                }
             }
             expressionCopy = expressionCopy.replace(previous, "");
         }
         return output;
     },
-    // todo: allow for functions like f(x) or cos(x)
     // https://en.wikipedia.org/wiki/Shunting_yard_algorithm
-    toPostfixNotation(expression) {
-        if (!/^([-+]*[\da-zA-Z]+)([-+*/^(]\(*[-+]*[\da-zA-Z]+\)*)*$/.test(expression)) {
-            throw new Error("Invalid expression!");
-        }
-        let tokenArray = this.toTokenArray(expression);
+    toPostfixNotation(expression, constants = new Map(), userFunctions = new Map()) {
+        expression = expression.replace(/\s/g, "");
+        // if (!/^([-+]*(([a-zA-Z]|\d+.?\d*)+'*)*)([-+*/^(]\(*[-+]*(([a-zA-Z]|\d+.?\d*)+'*)*\)*)*$/.test(expression)) {
+        //     throw new Error("Invalid expression!")
+        // }
+        let tokenArray = this.toTokenArray(expression, constants, userFunctions);
         // https://en.wikipedia.org/wiki/Reverse_Polish_notation
         let postfixOutput = [];
         let operatorStack = [];
         while (tokenArray.length > 0) {
             if (tokenArray[0] instanceof Operand) {
                 postfixOutput.push(tokenArray[0]);
+            }
+            else if (tokenArray[0] instanceof TFunction) {
+                operatorStack.push(tokenArray[0]);
             }
             else if (tokenArray[0] instanceof Operator) {
                 while (true) {
@@ -145,8 +217,11 @@ const mathParser = {
                 if (operatorStack.at(-1).value === "(") {
                     operatorStack.pop();
                 }
-                //         if there is a function token at the top of the operator stack, then:
-                //             pop the function from the operator stack into the output queue
+                // todo: allow for functions like f(x) or cos(x)
+                if (operatorStack.at(-1) instanceof TFunction) {
+                    // @ts-ignore
+                    postfixOutput.push(operatorStack.pop());
+                }
             }
             tokenArray.shift();
         }
@@ -160,7 +235,60 @@ const mathParser = {
             }
         }
         return postfixOutput;
+    },
+    // Helper method to convert a string into a js Function
+    toFunction(name, expression, constants = new Map(), userFunctions = new Map()) {
+        return new TFunction(name, (x) => {
+            let variable = "x";
+            while (constants.has(variable)) {
+                variable += "'";
+            }
+            constants.set(variable, x.toString());
+            return this.calculate(expression.replace(/x/g, variable), constants, userFunctions);
+        });
+    },
+    calculate(expression, constants = new Map(), userFunctions = new Map()) {
+        let postfix = this.toPostfixNotation(expression, constants, userFunctions);
+        while (true) {
+            let foundOperator = false;
+            for (let i = 0; i < postfix.length; i++) {
+                if (postfix[i] instanceof Operator) {
+                    const operator = postfix[i];
+                    foundOperator = true;
+                    try {
+                        const result = operator.apply(parseFloat(postfix[i - 2].value), parseFloat(postfix[i - 1].value));
+                        postfix.splice(i - 2, 3, new Operand(result.toString()));
+                    }
+                    catch (e) {
+                        throw new Error("Invalid expression! There aren't enough operands");
+                    }
+                    break;
+                }
+                else if (postfix[i] instanceof TFunction) {
+                    const func = postfix[i];
+                    foundOperator = true;
+                    const args = postfix.slice(i - func.onApply.length, i).map(value => parseFloat(value.value));
+                    const result = func.apply(args);
+                    postfix.splice(i - func.onApply.length, func.onApply.length + 1, new Operand(result.toString()));
+                    break;
+                }
+            }
+            if (!foundOperator)
+                break;
+        }
+        if (postfix.length > 1) {
+            throw new Error("Invalid expression! There are too many operands");
+        }
+        return parseFloat(postfix[0].value);
     }
 };
 exports.mathParser = mathParser;
-console.log(mathParser.toPostfixNotation("3+4*2/(1-5)^2^3"));
+const userFunctions = new Map([
+    ["f", "2x+3"]
+]);
+const constants = new Map([
+    ["x", "6+5"],
+    ["y", "2x+3"],
+    ["z", "5x-2y"]
+]);
+console.log(mathParser.calculate("max(2, 3)", constants, userFunctions));
--- pages/js-graphing/src/math-parser.ts
+++ pages/js-graphing/src/math-parser.ts
@@ -7,15 +7,35 @@ interface Token {
     readonly value: string;
 }
 
+class TFunction implements Token {
+    readonly value: string;
+    readonly onApply: Function;
+
+    constructor(value: string, onApply: Function) {
+        this.value = value;
+        this.onApply = onApply;
+    }
+
+    apply(args: Array<number>) {
+        return this.onApply.apply(null, args);
+    }
+}
+
 class Operator implements Token {
     readonly value: string;
     readonly precedence: Number;
     readonly associativity: Associativity;
+    readonly onApply: Function;
 
-    constructor(value: string, precedence: Number, associativity: Associativity) {
+    constructor(value: string, precedence: Number, associativity: Associativity, onApply: Function) {
         this.value = value;
         this.precedence = precedence;
         this.associativity = associativity;
+        this.onApply = onApply;
+    }
+
+    apply(a: number, b: number): number {
+        return this.onApply(a, b);
     }
 }
 
@@ -35,31 +55,48 @@ class Operand implements Token {
     }
 }
 
-const parentheses = new Map([
+const parentheses: Map<string, Parenthesis> = new Map([
     ["(", new Parenthesis("(")],
     [")", new Parenthesis(")")]
 ])
 
-const operators = new Map([
+const operators: Map<string, Operator> = new Map([
     // Exponentiation
-    ["^", new Operator("^", 4, Associativity.RIGHT)],
+    ["^", new Operator("^", 4, Associativity.RIGHT, (a: number, b: number) => Math.pow(a, b))],
         // ["**", new Operator("**", 4, Associativity.RIGHT)],
     // Multiplication
-    ["*", new Operator("*", 3, Associativity.LEFT)],
+    ["*", new Operator("*", 3, Associativity.LEFT, (a: number, b: number) => a * b)],
         // ["×", new Operator("×", 3, Associativity.LEFT)],
     // Division
-    ["/", new Operator("/", 3, Associativity.LEFT)],
+    ["/", new Operator("/", 3, Associativity.LEFT, (a: number, b: number) => a / b)],
         // ["÷", new Operator("÷", 3, Associativity.LEFT)],
         // [":", new Operator(":", 3, Associativity.LEFT)],
     // Addition
-    ["+", new Operator("+", 2, Associativity.LEFT)],
+    ["+", new Operator("+", 2, Associativity.LEFT, (a: number, b: number) => a + b)],
     // Subtraction
-    ["-", new Operator("-", 2, Associativity.LEFT)],
+    ["-", new Operator("-", 2, Associativity.LEFT, (a: number, b: number) => a - b)],
 ]);
+
+const functions: Map<string, TFunction> = new Map([]);
+
+const descriptors = Object.getOwnPropertyDescriptors(Math);
+const keys = Object.keys(descriptors);
+for (let i = 0; i < keys.length; i++) {
+    let value = descriptors[keys[i]].value;
+    if (typeof(value) === "function") functions.set(keys[i], new TFunction(keys[i], value))
+}
 
 function isNumber(str: string) {
     try {
         return str.replace(str.match(/\d+\.*\d*/)![0], "").length === 0;
+    } catch (e) {
+        return false;
+    }
+}
+
+function isVariable(str: string) {
+    try {
+        return str.replace(str.match(/[a-zA-Z]+'*/)![0], "").length === 0;
     } catch (e) {
         return false;
     }
@@ -75,11 +112,13 @@ function isOperator(str: string) {
 }
 
 const mathParser = {
-    // 15 - 8 * 14.6
-    // 3+4*2/(1-5)^2^3
     // todo: support numbers written like -5 or +10
-    toTokenArray(expression: string): Array<Token> {
-        let expressionCopy: string = expression.replace(/\s/g, "") + " ";
+    toTokenArray(expression: string, constants: Map<string, string> = new Map(),
+                 userFunctions: Map<string, TFunction | string> = new Map()): Array<Token> {
+        let expressionCopy: string = expression
+                .replace(/\s/g, "")
+                .replace(/(?<=\d)([a-zA-Z]'*)/g, "*$1")
+            + " ";
         let output: Array<Token> = [];
 
         while (expressionCopy.length > 0) {
@@ -88,22 +127,57 @@ const mathParser = {
             for (let i = 1; i <= expressionCopy.length; i++) {
                 const slice: string = expressionCopy.slice(0, i);
 
-                if (isNumber(previous) && !isNumber(slice)) {
+                if ((isNumber(previous) && !isNumber(slice))) {
                     output.push(new Operand(previous));
+
                     break;
-                } else if (isOperator(slice)) {
-                    const operator: Operator | undefined = operators.get(slice);
+                } else if (isVariable(previous) && !isVariable(slice)) {
+                    if (slice.endsWith("(")) { // check if it's a function
+                        if (functions.has(previous)) { // @ts-ignore
+                            output.push(functions.get(previous));
+
+                            break;
+                        } else if (userFunctions.has(previous)) {
+                            // @ts-ignore
+                            let func: string | TFunction = userFunctions.get(previous);
+                            if (typeof(func) === "string") {
+                                func = this.toFunction(previous, func, constants, userFunctions);
+                            }
+
+                            output.push(func);
+
+                            break;
+                        } else {
+                            throw new Error("Found invalid function " + previous);
+                        }
+                    } else if (constants.has(previous)) {
+                        // @ts-ignore
+                        let constant: string = constants.get(previous);
+                        const constantsCopy = constants;
+                        constantsCopy.delete(constant)
+                        output.push(...this.toTokenArray("(" + constant + ")", constantsCopy));
+
+                        break;
+                    } else {
+                        throw new Error("Could not find variable \"" + previous + "\"!")
+                    }
+                } else if (isOperator(previous) && !isOperator(slice)) {
+                    const operator: Operator | undefined = operators.get(previous);
                     if (operator != undefined) output.push(operator);
-                    previous = slice;
+
                     break;
                 } else if (isParentheses(slice)) {
                     const parenthesis: Parenthesis | undefined = parentheses.get(slice);
                     if (parenthesis != undefined) output.push(parenthesis);
-                    previous = slice;
-                    break;
-                }
 
-                previous = slice;
+                    previous = slice;
+
+                    break;
+                } else if (slice.length === expressionCopy.length) { // If nothing was found in the entire string
+                    expressionCopy = expressionCopy.slice(1);
+                } else { // If nothing was found for the current slice
+                    previous = slice;
+                }
             }
 
             expressionCopy = expressionCopy.replace(previous, "");
@@ -112,26 +186,30 @@ const mathParser = {
         return output;
     },
 
-    // todo: allow for functions like f(x) or cos(x)
     // https://en.wikipedia.org/wiki/Shunting_yard_algorithm
-    toPostfixNotation(expression: string): Array<Token> {
-        if (!/^([-+]*[\da-zA-Z]+)([-+*/^(]\(*[-+]*[\da-zA-Z]+\)*)*$/.test(expression)) {
-            throw new Error("Invalid expression!")
-        }
+    toPostfixNotation(expression: string, constants: Map<string, string> = new Map(),
+                      userFunctions: Map<string, TFunction | string> = new Map()): Array<Token> {
+        expression = expression.replace(/\s/g, "");
 
-        let tokenArray: Array<Token> = this.toTokenArray(expression);
+        // if (!/^([-+]*(([a-zA-Z]|\d+.?\d*)+'*)*)([-+*/^(]\(*[-+]*(([a-zA-Z]|\d+.?\d*)+'*)*\)*)*$/.test(expression)) {
+        //     throw new Error("Invalid expression!")
+        // }
+
+        let tokenArray: Array<Token> = this.toTokenArray(expression, constants, userFunctions);
 
         // https://en.wikipedia.org/wiki/Reverse_Polish_notation
         let postfixOutput: Array<Token> = [];
-        let operatorStack: Array<Operator | Parenthesis> = [];
+        let operatorStack: Array<Operator | Parenthesis | TFunction> = [];
 
         while (tokenArray.length > 0) {
             if (tokenArray[0] instanceof Operand) {
                 postfixOutput.push(tokenArray[0]);
+            } else if (tokenArray[0] instanceof TFunction) {
+                operatorStack.push(tokenArray[0]);
             } else if (tokenArray[0] instanceof Operator) {
                 while (true) {
                     // @ts-ignore
-                    const top: Operator | Parenthesis = operatorStack.at(-1);
+                    const top: Operator | Parenthesis | TFunction = operatorStack.at(-1);
                     if (top instanceof Operator
                         && (top.precedence > tokenArray[0].precedence
                             || (top.precedence === tokenArray[0].precedence
@@ -164,8 +242,10 @@ const mathParser = {
                     operatorStack.pop();
                 }
 
-                //         if there is a function token at the top of the operator stack, then:
-                //             pop the function from the operator stack into the output queue
+                if (operatorStack.at(-1) instanceof TFunction) {
+                    // @ts-ignore
+                    postfixOutput.push(operatorStack.pop());
+                }
             }
 
             tokenArray.shift();
@@ -181,9 +261,78 @@ const mathParser = {
         }
 
         return postfixOutput;
+    },
+
+    // Helper method to convert a string into a js Function
+    toFunction(name: string, expression: string, constants: Map<string, string> = new Map(),
+               userFunctions: Map<string, TFunction | string> = new Map()): TFunction {
+        return new TFunction(name, (x: number) => {
+            let variable = "x";
+
+            while (constants.has(variable)) {
+                variable += "'";
+            }
+
+            constants.set(variable, x.toString());
+
+            return this.calculate(expression.replace(/x/g, variable), constants, userFunctions);
+        });
+    },
+
+    calculate(expression: string, constants: Map<string, string> = new Map(),
+              userFunctions: Map<string, TFunction | string> = new Map()): number {
+        let postfix: Array<Token> = this.toPostfixNotation(expression, constants, userFunctions);
+        while (true) {
+            let foundOperator = false;
+
+            for (let i = 0; i < postfix.length; i++) {
+                if (postfix[i] instanceof Operator) {
+                    const operator: Operator = <Operator>postfix[i];
+                    foundOperator = true;
+
+                    try {
+                        const result = operator.apply(parseFloat(postfix[i - 2].value), parseFloat(postfix[i - 1].value));
+
+                        postfix.splice(i - 2, 3, new Operand(result.toString()));
+                    } catch (e) {
+                        throw new Error("Invalid expression! There aren't enough operands")
+                    }
+
+                    break;
+                } else if (postfix[i] instanceof TFunction) {
+                    const func: TFunction = <TFunction>postfix[i];
+                    foundOperator = true;
+
+                    const args: number[] = postfix.slice(i - func.onApply.length, i).map(value => parseFloat(value.value));
+                    const result = func.apply(args)
+
+                    postfix.splice(i - func.onApply.length, func.onApply.length + 1,
+                        new Operand(result.toString()))
+
+                    break;
+                }
+            }
+
+            if (!foundOperator) break;
+        }
+
+        if (postfix.length > 1) {
+            throw new Error("Invalid expression! There are too many operands")
+        }
+
+        return parseFloat(postfix[0].value);
     }
 }
 
-console.log(mathParser.toPostfixNotation("3+4*2/(1-5)^2^3"));
+const userFunctions = new Map([
+    ["f", "2x+3"]
+])
+const constants = new Map([
+    ["x", "6+5"],
+    ["y", "2x+3"],
+    ["z", "5x-2y"]
+])
+
+console.log(mathParser.calculate("max(2, 3)", constants, userFunctions));
 
 export { mathParser }
