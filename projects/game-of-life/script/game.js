ee3b93f793da9be52dd404e3d93f9ec1109f9f42
Discusser <47938380+Discusser@users.noreply.github.com>
2024-08-01 12:54:02 +0200

    Updates

--- projects/game-of-life/script/game.js
+++ projects/game-of-life/script/game.js
@@ -47,51 +47,97 @@ export class GameInfo {
         this._gamePaused = value;
         this.game.updateGameStatus();
     }
+    get generationInterval() {
+        return this._generationInterval;
+    }
+    set generationInterval(value) {
+        if (isNaN(value) || !isFinite(value) || value == null)
+            return;
+        this._generationInterval = Math.max(value, 1);
+    }
     constructor(game) {
         this._generations = 0;
         this._gamePaused = true;
-        this.generationInterval = 200; // this value is stored in ms
+        this._generationInterval = 200; // this value is stored in ms
         this.cellSize = 16;
         this.cellBorderColor = "rgba(100, 100, 100, 0.3)";
         this.cellBackgroundColor = "black";
         this.cellLiveBackgroundColor = "white";
         this.cellHoverBackgroundColor = "lightgray";
         this.drawGrid = true;
+        this.drawMode = DrawMode.Create;
+        this.drawHover = true;
         this.game = game;
     }
 }
+class GameRenderer {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+    }
+}
+export var DrawMode;
+(function (DrawMode) {
+    DrawMode[DrawMode["Create"] = 0] = "Create";
+    DrawMode[DrawMode["Erase"] = 1] = "Erase";
+})(DrawMode || (DrawMode = {}));
+var MouseButton;
+(function (MouseButton) {
+    MouseButton[MouseButton["Left"] = 0] = "Left";
+    MouseButton[MouseButton["Middle"] = 1] = "Middle";
+    MouseButton[MouseButton["Right"] = 2] = "Right";
+})(MouseButton || (MouseButton = {}));
 export class Game {
-    constructor(canvas, gameStatus, generationCount, populationCount) {
-        this.elapsed = 0;
+    constructor(canvas, gameStatus, generationCount, populationCount, buttonDrawMode) {
+        this.renderElapsed = 0;
+        this.simulationElapsed = 0;
         this.nextLiveCells = new GeneralSet();
         this.liveCells = new GeneralSet();
         this.maxFpsValues = 10;
         this.fpsValues = new Array(this.maxFpsValues);
-        this.canPan = false;
         this.scale = 1;
         this.maxScale = 3;
         this.minScale = 0.05;
         this.mouseMovement = [0, 0];
         this.info = new GameInfo(this);
-        this.canvas = canvas;
+        this.renderer = new GameRenderer(canvas);
         this.gameStatus = gameStatus;
         this.generationCount = generationCount;
         this.populationCount = populationCount;
-        this.ctx = canvas.getContext("2d");
-        this.canvas.addEventListener("contextmenu", (event) => this.onContextMenuOnCanvas(event));
-        this.canvas.addEventListener("scroll", (event) => this.onScrollOnCanvas(event));
-        this.canvas.addEventListener("wheel", (event) => this.onWheelOnCanvas(event));
-        this.canvas.addEventListener("mousedown", (event) => this.onMouseDownOnCanvas(event));
-        this.canvas.addEventListener("mouseup", (event) => this.onMouseUpOnCanvas(event));
-        this.canvas.addEventListener("mousemove", (event) => this.onMouseMoveOnCanvas(event));
-        this.canvas.addEventListener("mouseleave", (event) => this.onMouseLeaveCanvas(event));
-        this.canvas.addEventListener("click", (event) => this.onClickOnCanvas(event));
+        this.buttonDrawMode = buttonDrawMode;
+        this.renderer.canvas.addEventListener("contextmenu", (event) => this.onContextMenuOnCanvas(event));
+        this.renderer.canvas.addEventListener("wheel", (event) => this.onWheelOnCanvas(event));
+        this.renderer.canvas.addEventListener("mousedown", (event) => this.onMouseDownOnCanvas(event));
+        this.renderer.canvas.addEventListener("mouseup", (event) => this.onMouseUpOnCanvas(event));
+        this.renderer.canvas.addEventListener("mousemove", (event) => this.onMouseMoveOnCanvas(event));
+        this.renderer.canvas.addEventListener("mouseleave", (event) => this.onMouseLeaveCanvas(event));
+        this.renderer.canvas.addEventListener("click", (event) => this.onClickOnCanvas(event));
+    }
+    startGame() {
+        setInterval(() => this.update(), 1);
+        requestAnimationFrame((t) => this.drawFrame(t));
+    }
+    update() {
+        const timestamp = performance.now();
+        if (this.simulationPreviousTimestamp === undefined)
+            this.simulationPreviousTimestamp = timestamp;
+        const deltaTime = timestamp - this.simulationPreviousTimestamp;
+        this.simulationElapsed += deltaTime;
+        if (this.simulationElapsed >= this.info.generationInterval) {
+            this.simulationElapsed = this.simulationElapsed % this.info.generationInterval;
+            if (!this.info.gamePaused)
+                this.runGeneration();
+        }
+        this.simulationPreviousTimestamp = timestamp;
     }
     // HTML functions
     updateGameStatus() {
-        this.gameStatus.textContent = this.info.gamePaused
-            ? "Game paused"
-            : `Game playing at ${this.getAverageFps().toFixed(1)} fps`;
+        const fpsText = `at ${this.getAverageFps().toFixed(1)} fps`;
+        this.gameStatus.textContent = this.info.gamePaused ? `Game paused ${fpsText}` : `Game playing ${fpsText}`;
+    }
+    switchDrawMode() {
+        this.info.drawMode = this.info.drawMode == DrawMode.Create ? DrawMode.Erase : DrawMode.Create;
+        this.buttonDrawMode.value = this.info.drawMode == DrawMode.Create ? "Create mode" : "Erase mode";
     }
     updateGameStatistics() {
         this.generationCount.textContent = this.info.generations.toString();
@@ -101,12 +147,12 @@ export class Game {
     drawGrid() {
         if (!this.info.drawGrid || this.scale <= 0.5)
             return;
-        const transform = this.ctx.getTransform();
-        const x1 = Math.floor(-Math.abs(transform.e / this.info.cellSize / this.scale));
-        const y1 = Math.floor(-Math.abs(transform.f / this.info.cellSize / this.scale));
-        const x2 = Math.ceil(this.canvas.width / this.info.cellSize / this.scale - x1);
-        const y2 = Math.ceil(this.canvas.height / this.info.cellSize / this.scale - y1);
-        this.ctx.strokeStyle = this.info.cellBorderColor;
+        const transform = this.renderer.ctx.getTransform();
+        const x1 = Math.floor(-transform.e / this.info.cellSize / this.scale);
+        const y1 = Math.floor(-transform.f / this.info.cellSize / this.scale);
+        const x2 = Math.ceil(this.renderer.canvas.width / this.info.cellSize / this.scale) + x1;
+        const y2 = Math.ceil(this.renderer.canvas.height / this.info.cellSize / this.scale) + y1;
+        this.renderer.ctx.strokeStyle = this.info.cellBorderColor;
         for (let x = x2; x >= x1; x--) {
             for (let y = y2; y >= y1; y--) {
                 this.drawCellBorder(x, y, this.info.cellSize);
@@ -114,62 +160,65 @@ export class Game {
         }
     }
     drawCells() {
-        this.ctx.strokeStyle = this.info.cellBorderColor;
-        this.ctx.fillStyle = this.info.cellLiveBackgroundColor;
+        this.renderer.ctx.strokeStyle = this.info.cellBorderColor;
+        this.renderer.ctx.fillStyle = this.info.cellLiveBackgroundColor;
         this.liveCells.map.forEach((pos) => {
-            this.drawCell(pos.x, pos.y, this.info.cellSize);
+            this.drawCellNoBorder(pos.x, pos.y, this.info.cellSize);
         });
-        if (this.hoveredCell) {
-            this.ctx.fillStyle = this.info.cellHoverBackgroundColor;
+        this.liveCells.map.forEach((pos) => {
+            this.drawCellBorder(pos.x, pos.y, this.info.cellSize);
+        });
+        if (this.hoveredCell && this.info.drawHover) {
+            this.renderer.ctx.fillStyle = this.info.cellHoverBackgroundColor;
             this.drawCell(this.hoveredCell.x, this.hoveredCell.y, this.info.cellSize);
         }
     }
     drawCellBorder(column, row, cellSize) {
-        this.ctx.beginPath();
-        this.ctx.rect(column * cellSize, row * cellSize, cellSize, cellSize);
-        this.ctx.closePath();
-        this.ctx.stroke();
+        this.renderer.ctx.beginPath();
+        this.renderer.ctx.rect(column * cellSize, row * cellSize, cellSize, cellSize);
+        this.renderer.ctx.closePath();
+        this.renderer.ctx.stroke();
+    }
+    drawCellNoBorder(column, row, cellSize) {
+        this.renderer.ctx.beginPath();
+        this.renderer.ctx.rect(column * cellSize, row * cellSize, cellSize, cellSize);
+        this.renderer.ctx.closePath();
+        this.renderer.ctx.fill();
     }
     drawCell(column, row, cellSize) {
-        this.drawCellBorder(column, row, cellSize);
-        this.ctx.fill();
+        this.renderer.ctx.beginPath();
+        this.renderer.ctx.rect(column * cellSize, row * cellSize, cellSize, cellSize);
+        this.renderer.ctx.closePath();
+        this.renderer.ctx.stroke();
+        this.renderer.ctx.fill();
     }
     drawFrame(timestamp) {
-        if (this.start === undefined)
-            this.start = timestamp;
-        if (this.previousTimestamp === undefined)
-            this.previousTimestamp = timestamp;
-        const deltaTime = timestamp - this.previousTimestamp;
-        this.elapsed += deltaTime;
+        if (this.renderPreviousTimestamp === undefined)
+            this.renderPreviousTimestamp = timestamp;
+        const deltaTime = timestamp - this.renderPreviousTimestamp;
+        this.renderElapsed += deltaTime;
         if (this.fpsValues.length > this.maxFpsValues)
             this.fpsValues.splice(0, this.fpsValues.length - this.maxFpsValues);
         if (deltaTime != 0)
             this.fpsValues.push((1 / deltaTime) * 1000);
-        const previousTransform = this.ctx.getTransform();
-        this.ctx.setTransform(new DOMMatrix());
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.setTransform(previousTransform);
+        const previousTransform = this.renderer.ctx.getTransform();
+        this.renderer.ctx.setTransform(new DOMMatrix());
+        this.renderer.ctx.clearRect(0, 0, this.renderer.canvas.width, this.renderer.canvas.height);
+        this.renderer.ctx.setTransform(previousTransform);
         this.drawGrid();
         this.drawCells();
-        if (this.elapsed >= this.info.generationInterval) {
-            this.elapsed = this.elapsed % this.info.generationInterval;
-            if (!this.info.gamePaused)
-                this.runGeneration();
-        }
-        this.previousTimestamp = timestamp;
         this.updateGameStatus();
+        this.renderPreviousTimestamp = timestamp;
         requestAnimationFrame((t) => this.drawFrame(t));
     }
     // Event handlers
     onContextMenuOnCanvas(event) {
         event.preventDefault();
     }
-    onScrollOnCanvas(event) {
-        event.preventDefault();
-    }
     onWheelOnCanvas(event) {
-        const transform = this.ctx.getTransform();
-        const transformed = this.transformCoordinates(this.canvas.width / 2, this.canvas.height / 2);
+        event.preventDefault();
+        const transform = this.renderer.ctx.getTransform();
+        const transformed = this.transformCoordinates(this.renderer.canvas.width / 2, this.renderer.canvas.height / 2);
         let scaleMultiplier = 1 - 0.2 * Math.sign(event.deltaY);
         const newScale = transform.a * scaleMultiplier;
         if (newScale > this.maxScale)
@@ -178,28 +227,25 @@ export class Game {
             scaleMultiplier = this.minScale / transform.a;
         transform.scaleSelf(scaleMultiplier, scaleMultiplier, 1, transformed.x, transformed.y);
         this.scale = transform.a;
-        this.ctx.setTransform(transform);
+        this.renderer.ctx.setTransform(transform);
     }
     onMouseDownOnCanvas(event) {
         this.mouseButtonHeld = event.button;
-        if (this.mouseButtonHeld == 2)
-            this.canPan = true;
     }
     onMouseUpOnCanvas(_) {
-        if (this.mouseButtonHeld == 2) {
-            this.canPan = false;
+        if (this.mouseButtonHeld == MouseButton.Right) {
             this.mouseMovement = [0, 0];
         }
         this.mouseButtonHeld = undefined;
     }
     onMouseMoveOnCanvas(event) {
         // Don't display hovered cell when holding right click
-        if (this.mouseButtonHeld == 2)
+        if (this.mouseButtonHeld == MouseButton.Right)
             this.hoveredCell = undefined;
         else
             this.hoveredCell = this.getCellAtCoordinates(event.offsetX, event.offsetY);
-        if (this.canPan) {
-            const transform = this.ctx.getTransform();
+        if (this.mouseButtonHeld == MouseButton.Right) {
+            const transform = this.renderer.ctx.getTransform();
             const scaledSize = this.info.cellSize * this.scale;
             this.mouseMovement[0] += event.movementX;
             this.mouseMovement[1] += event.movementY;
@@ -211,20 +257,27 @@ export class Game {
                 transform.f += Math.trunc(this.mouseMovement[1] / scaledSize) * scaledSize;
                 this.mouseMovement[1] = this.mouseMovement[1] % scaledSize;
             }
-            this.ctx.setTransform(transform);
+            this.renderer.ctx.setTransform(transform);
+        }
+        else if (this.mouseButtonHeld == MouseButton.Left && this.hoveredCell) {
+            if (this.info.drawMode == DrawMode.Create) {
+                this.liveCells.add(this.hoveredCell);
+            }
+            else if (this.info.drawMode == DrawMode.Erase) {
+                this.liveCells.delete(this.hoveredCell);
+            }
         }
     }
     onMouseLeaveCanvas(_) {
         this.hoveredCell = undefined;
         this.mouseButtonHeld = undefined;
-        this.canPan = false;
     }
     onClickOnCanvas(event) {
         const position = this.getCellAtCoordinates(event.offsetX, event.offsetY);
-        if (this.liveCells.has(position)) {
+        if (this.info.drawMode == DrawMode.Erase) {
             this.liveCells.delete(position);
         }
-        else {
+        else if (this.info.drawMode == DrawMode.Create) {
             this.liveCells.add(position);
         }
         this.updateGameStatistics();
@@ -233,16 +286,17 @@ export class Game {
     getAverageFps() {
         if (this.fpsValues.length == 0)
             return 0;
-        return this.fpsValues.reduce((prev, curr) => prev + curr) / this.fpsValues.length;
+        return this.fpsValues.reduce((prev, curr) => prev + curr, 0) / this.fpsValues.length;
     }
     resetGame() {
         this.info.generations = 0;
         this.info.gamePaused = true;
         this.liveCells = new GeneralSet();
         this.nextLiveCells = new GeneralSet();
-        this.previousTimestamp = undefined;
-        this.start = undefined;
-        this.elapsed = 0;
+        this.renderPreviousTimestamp = undefined;
+        this.renderElapsed = 0;
+        this.simulationPreviousTimestamp = undefined;
+        this.simulationElapsed = 0;
         this.updateGameStatistics();
         this.updateGameStatus();
     }
@@ -297,15 +351,15 @@ export class Game {
         return this.liveCells.has(pos);
     }
     getCellAtCoordinates(x, y) {
-        x = clamp(x, 0, this.canvas.width);
-        y = clamp(y, 0, this.canvas.height);
+        x = clamp(x, 0, this.renderer.canvas.width);
+        y = clamp(y, 0, this.renderer.canvas.height);
         const transformed = this.transformCoordinates(x, y);
         const column = Math.floor(transformed.x / this.info.cellSize);
         const row = Math.floor(transformed.y / this.info.cellSize);
         return new Position(column, row);
     }
     transformCoordinates(x, y) {
-        const transform = this.ctx.getTransform().inverse();
+        const transform = this.renderer.ctx.getTransform().inverse();
         return {
             x: transform.a * x + transform.c * y + transform.e,
             y: transform.b * x + transform.d * y + transform.f,
--- projects/game-of-life/script/index.js
+++ projects/game-of-life/script/index.js
@@ -4,7 +4,9 @@ const gameContainer = document.querySelector(".game-container");
 const gameStatus = document.querySelector(".game-status");
 const generationCount = document.querySelector(".generation-count");
 const populationCount = document.querySelector(".population-count");
-const game = new Game(gameContainer, gameStatus, generationCount, populationCount);
+const buttonDrawMode = document.querySelector(".button-draw-mode");
+const keysPressed = new Map();
+const game = new Game(gameContainer, gameStatus, generationCount, populationCount, buttonDrawMode);
 (_a = document.querySelector(".button-play")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
     game.info.gamePaused = false;
 });
@@ -20,6 +22,29 @@ const game = new Game(gameContainer, gameStatus, generationCount, populationCoun
 (_e = document.querySelector(".button-toggle-grid")) === null || _e === void 0 ? void 0 : _e.addEventListener("click", () => {
     game.info.drawGrid = !game.info.drawGrid;
 });
+buttonDrawMode.addEventListener("click", () => game.switchDrawMode());
+window.addEventListener("keydown", (event) => {
+    if (!keysPressed.get(event.key)) {
+        keysPressed.set(event.key, true);
+        switch (event.key) {
+            case "f":
+                game.switchDrawMode();
+                break;
+            case "g":
+                game.info.drawGrid = !game.info.drawGrid;
+                break;
+            case "v":
+                game.info.drawHover = !game.info.drawHover;
+                break;
+            case "p":
+                game.info.gamePaused = !game.info.gamePaused;
+                break;
+        }
+    }
+});
+window.addEventListener("keyup", (event) => {
+    keysPressed.set(event.key, false);
+});
 (_f = document.querySelector(".option-generation-interval")) === null || _f === void 0 ? void 0 : _f.addEventListener("input", (event) => {
     if (event instanceof InputEvent) {
         const inputEvent = event;
@@ -28,4 +53,4 @@ const game = new Game(gameContainer, gameStatus, generationCount, populationCoun
         }
     }
 });
-requestAnimationFrame((t) => game.drawFrame(t));
+game.startGame();
